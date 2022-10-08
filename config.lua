19d4c985d0a6ef08234177c06da7f4218beebf6c
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2022-10-08 12:45:41 +0330

    feat: added `vim-surround` plugin

--- config.lua
+++ config.lua
@@ -173,7 +173,14 @@ lvim.plugins = {
       cmd = "TroubleToggle",
     },
     {
-      'michaeljsmith/vim-indent-object'
+      "michaeljsmith/vim-indent-object"
+    },
+    {
+      "tpope/vim-surround",
+      -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+      -- setup = function()
+        --  vim.o.timeoutlen = 500
+      -- end
     },
 }
 
